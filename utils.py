062c6af72c4f2abdea5c2dc89003bc6072d31aab
s.ranjbar <sadegh.r1404@gmail.com>
2021-02-06 16:07:14 +0330

    Resize change

--- utils.py
+++ utils.py
@@ -5,8 +5,10 @@ from tensorflow.keras import backend as K
 @tf.function(experimental_relax_shapes=True)
 def read_jpeg_image(img_path):
     image = tf.io.read_file(img_path)
-    image = tf.image.resize(image,(1024,1024))
+    
     image = tf.image.decode_jpeg(image, channels=3)
+    image = tf.image.resize(image,[1024,1024])
+    # print(image.shape)
     return image
 
 
